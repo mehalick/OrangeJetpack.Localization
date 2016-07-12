b494ac30dbb4cb31c094eba088908245b38bc4ed
Andy Mehalick <andy.mehalick@orangejetpack.com>
2016-07-12 14:47:27 +0100

    reordered public methods; added test for new Localize() method

--- src/OrangeJetpack.Localization/LocalizedContentExtensions.cs
+++ src/OrangeJetpack.Localization/LocalizedContentExtensions.cs
@@ -48,6 +48,61 @@ namespace OrangeJetpack.Localization
             return item;
         }
 
+        /// <summary>
+        /// Returns an item with one or more localized properties deserialized and set to specified language. 
+        /// </summary>
+        /// <param name="item">The items to localize.</param>
+        /// <param name="language">The language to use for localization.</param>
+        /// <param name="properties">The collection of localized properties to process.</param>
+        /// <remarks>If the specified language is not found the default language or first in list is used.</remarks>
+        public static T Localize<T>(this T item, string language, params Expression<Func<T, string>>[] properties) where T : class, ILocalizable
+        {
+            if (item == null)
+            {
+                return null;
+            }
+
+            LocalizeProperties(item, language, properties);
+
+            return item;
+        }
+
+        /// <summary>
+        /// Returns a collection with one or more localized properties deserialized and set to specified language. 
+        /// </summary>
+        /// <param name="items">The collection of items to localize.</param>
+        /// <param name="language">The language to use for localization.</param>
+        /// <param name="depth">The depth to which child properties should be localized.</param>
+        /// <remarks>If the specified language is not found the default language or first in list is used.</remarks>
+        public static IEnumerable<T> Localize<T>(this IEnumerable<T> items, string language, LocalizationDepth depth = LocalizationDepth.Shallow) where T : class, ILocalizable
+        {
+            foreach (var item in items)
+            {
+                LocalizeProperties(item, language);
+                LocalizeChildren(item, language, depth);
+                LocalizeCollections(item, language, depth);
+
+                yield return item;
+            }
+        }
+
+        /// <summary>
+        /// Returns a collection with one or more localized properties deserialized and set to specified language. 
+        /// </summary>
+        /// <param name="items">The collection of items to localize.</param>
+        /// <param name="language">The language to use for localization.</param>
+        /// <param name="properties">The collection of localized properties to process.</param>
+        /// <remarks>If the specified language is not found the default language or first in list is used.</remarks>
+        public static IEnumerable<T> Localize<T>(this IEnumerable<T> items, string language, params Expression<Func<T, string>>[] properties) where T : class, ILocalizable
+        {
+            foreach (var item in items)
+            {
+                LocalizeProperties(item, language, properties);
+
+                yield return item;
+            }
+        }
+
         private static void LocalizeChildren<T>(T item, string language, LocalizationDepth depth) where T : class, ILocalizable
         {
             if (depth == LocalizationDepth.Shallow)
@@ -97,25 +152,6 @@ namespace OrangeJetpack.Localization
             }
         }
 
-        /// <summary>
-        /// Returns a collection with one or more localized properties deserialized and set to specified language. 
-        /// </summary>
-        /// <param name="items">The collection of items to localize.</param>
-        /// <param name="language">The language to use for localization.</param>
-        /// <param name="depth">The depth to which child properties should be localized.</param>
-        /// <remarks>If the specified language is not found the default language or first in list is used.</remarks>
-        public static IEnumerable<T> Localize<T>(this IEnumerable<T> items, string language, LocalizationDepth depth = LocalizationDepth.Shallow) where T : class, ILocalizable
-        {
-            foreach (var item in items)
-            {
-                LocalizeProperties(item, language);
-                LocalizeChildren(item, language, depth);
-                LocalizeCollections(item, language, depth);
-
-                yield return item;
-            }
-        }
-
         private static void LocalizeProperties<T>(T item, string language)
         {
             var properties = item
@@ -139,42 +175,6 @@ namespace OrangeJetpack.Localization
 
                 var contentForLanguage = GetContentForLanguage(localizedContents, language);
                 propertyInfo.SetValue(item, contentForLanguage.Value, null);
-            }
-        }
-
-        /// <summary>
-        /// Returns an item with one or more localized properties deserialized and set to specified language. 
-        /// </summary>
-        /// <param name="item">The items to localize.</param>
-        /// <param name="language">The language to use for localization.</param>
-        /// <param name="properties">The collection of localized properties to process.</param>
-        /// <remarks>If the specified language is not found the default language or first in list is used.</remarks>
-        public static T Localize<T>(this T item, string language, params Expression<Func<T, string>>[] properties) where T : class, ILocalizable
-        {
-            if (item == null)
-            {
-                return null;
-            }
-
-            LocalizeProperties(item, language, properties);
-
-            return item;
-        }
-
-        /// <summary>
-        /// Returns a collection with one or more localized properties deserialized and set to specified language. 
-        /// </summary>
-        /// <param name="items">The collection of items to localize.</param>
-        /// <param name="language">The language to use for localization.</param>
-        /// <param name="properties">The collection of localized properties to process.</param>
-        /// <remarks>If the specified language is not found the default language or first in list is used.</remarks>
-        public static IEnumerable<T> Localize<T>(this IEnumerable<T> items, string language, params Expression<Func<T, string>>[] properties) where T : class, ILocalizable
-        {
-            foreach (var item in items)
-            {
-                LocalizeProperties(item, language, properties);
-
-                yield return item;
             }
         }
 
