741644bf71b7070aab918e621a9cfe7788ab237a
Andy Mehalick <andy.mehalick@orangejetpack.com>
2017-01-15 20:13:59 +0300

    preparing for release 2.0.0

--- build.ps1
+++ build.ps1
@@ -56,7 +56,7 @@ exec { & dotnet restore }
 
 Invoke-MSBuild
 
-$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 0 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
 exec { & dotnet test .\test\OrangeJetpack.Localization.Tests -c Release }
